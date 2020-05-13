43845d293983b27a876ff7466e4056e61514d9c2
Attiq Ur Rehman <attiqman007@gmail,com>
2020-05-13 15:49:40 +0500

    File updated

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,35 +1,59 @@
-import React from "react";
-import classes from './Input.module.css';
+import React from 'react';
 
+import classes from './Input.module.css'
 
-const input = (props) => {
+const input = ( props ) => {
     let inputElement = null;
-    switch (props.elementType) {
-        case ('input'):
-            inputElement =<input className={classes.InputElement} {...props.elementConfig} value={props.value} />
-            break;
-        case ('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value}/>
-            break;
-        case ('select'):
-            inputElement = <select
-                className={classes.InputElement}
-                {...props.elementConfig.options.map((option) => {
-                    return <option key={option.value} value={option.value}>
-                        {option.displayedValue}
-                    </option>
-                })}
-                value={props.value}> </select>
-            break;
-        default :
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value}/>
+    const inputClasses = [classes.InputElement];
+
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputClasses.push(classes.Invalid);
     }
-    return(
+
+    switch ( props.elementType ) {
+        case ( 'input' ):
+            inputElement = <input
+                className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value}
+                         />;
+            break;
+        case ( 'textarea' ):
+            inputElement = <textarea
+                className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value}
+                 />;
+            break;
+        case ( 'select' ):
+            inputElement = (
+                <select
+                    className={inputClasses.join(' ')}
+                    value={props.value}
+                    >
+                    {props.elementConfig.options.map(option => (
+                        <option key={option.value} value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}
+                </select>
+            );
+            break;
+        default:
+            inputElement = <input
+                className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value}
+                 />;
+    }
+
+    return (
         <div className={classes.Input}>
             <label className={classes.Label}>{props.label}</label>
             {inputElement}
         </div>
-    )
-}
+    );
+
+};
 
 export default input;
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,60 +8,58 @@ import Input from "../../../components/UI/Input/Input";
 class ContactData extends Component{
     state = {
         orderForm: {
-            customer: {
-                name: {
-                    elementType: 'input',
-                    elementConfig: {
-                        type: 'text',
-                        placeholder: 'Your Name'
-                    },
-                    value: ''
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
                 },
-                Country: {
-                    elementType: 'input',
-                    elementConfig: {
-                        type: 'text',
-                        placeholder: 'Your Country'
-                    },
-                    value: ''
-                },
-                street: {
-                    elementType: 'input',
-                    elementConfig: {
-                        type: 'text',
-                        placeholder: 'Your Street'
-                    },
-                    value: ''
-                },
-                zipCode: {
-                    elementType: 'input',
-                    elementConfig: {
-                        type: 'text',
-                        placeholder: 'ZipCode'
-                    },
-                    value: ''
-                },
-               email: {
-                    elementType: 'input',
-                    elementConfig: {
-                        type: 'email',
-                        placeholder: 'Email'
-                    },
-                    value: ''
-                },
-                deliveryMethod : {
-                    elementType: 'select',
-                    elementConfig: {
-                        options: [
-                            {value: 'fastest', displayedValue: 'Fastest'},
-                            {value: 'cheapest', displayedValue: 'Cheapest'}
-                        ]
-                    },
-                    value: ''
-                },
+                value: '',
             },
-            loading: false
-        }
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: '',
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: '',
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: '',
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-Mail'
+                },
+                value: '',
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
+                },
+                value: '',
+            }
+        },
+        loading: false
     }
     orderHandler = (event) => {
         event.preventDefault();
@@ -80,24 +78,22 @@ class ContactData extends Component{
     }
     render() {
         const formElementsArray = [];
-        for(let key in this.state.orderForm){
+        for (let key in this.state.orderForm) {
             formElementsArray.push({
                 id: key,
                 config: this.state.orderForm[key]
-            })
+            });
         }
-
         let form =(
-            <form >
-                {formElementsArray.map((formElement) => {
-                    return(
-                        <Input
-                         key={formElement.id}
-                         elementType={formElement.config.elementType}
-                         elementConfig={formElement.config.elementConfig}
-                         value={formElement.config.value}/>
-                    )
-                })}
+            <form onSubmit={this.orderHandler}>
+                {formElementsArray.map(formElement => (
+                    <Input
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                         />
+                ))}
                 <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
             </form>
         )
